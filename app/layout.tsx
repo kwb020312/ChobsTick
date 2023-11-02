b45f61cf2e5737fb54145f846a7e6b9ea42125b9
김우빈 <kwb020312@naver.com>
2023-11-02 21:49:52 +0900

    Feat: Apply Global CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Nav from "./(components)/Nav";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,8 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Nav />
-        {children}
+        <div className="flex flex-col h-screen max-h-screen">
+          <Nav />
+          <div className="flex-grow overflow-y-auto bg-page text-default-text">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
