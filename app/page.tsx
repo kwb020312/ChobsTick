38cb3b1ca0e1a15a3d3854aaa2c7adc4329000d8
김우빈 <kwb020312@naver.com>
2023-11-05 20:37:40 +0900

    Feat: Root Page Error Exception

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,10 @@ const getTickets = async () => {
       cache: "no-store",
     });
 
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
     return res.json();
   } catch (error) {
     console.log("Failed to get tickets", error);
